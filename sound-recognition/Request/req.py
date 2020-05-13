4aa08a50a9f104aef0febd902d5386300fab8b24
Merilla <Shemet-mini@mail.ru>
2020-05-13 08:44:40 +0300

    Modify repo

--- sound-recognition/Request/req.py
+++
@@ -1,5 +0,0 @@
-import requests
-files = {
-    'sample1': open('Samples/sample_02133277-0dc6-4b34-8a14-17a980c3307c.wav', 'rb'), 
-    'sample2': open('Samples/sample_63398534-adb8-4640-9645-e5e78290870e.wav', 'rb')}
-response = requests.post('http://127.0.0.1:5000/', files = files)
