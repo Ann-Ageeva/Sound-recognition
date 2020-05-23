edb2761eb0499b09a363a5f3bf2207fd39fbc315
Nikita <vashhue@bk.ru>
2020-05-23 15:50:48 +0300

    fix

--- code/BaseAlg.py
+++ code/BaseAlg.py
@@ -1,54 +1,61 @@
 import math as m
 
-import numpy
+import numpy as np
 from numpy.fft import rfft
 
 import librosa
 
-pathFirstMicro = ""
-pathSecondMicro = ""
 
+def test():
 
-def baseAlg():
-    maxRangeCor = 5500  # Диапозон маскимальной корреляции
-    u = 330  # Скорость звука
+    maxRangeCor = 10000  # Диапозон маскимальной корреляции
+    speed = 330  # Скорость звука
     frequency = 22000  # Частота дискретизации
     dist = 100  # Расстояние между микрофонами
 
+    pathFirstMicro = 'C:\\Users\\wwwvn\\Desktop\\тест\\detect\\right side about 20 degree\\mic 1 - 01 (Left).wav'
+    pathSecondMicro = 'C:\\Users\\wwwvn\\Desktop\\тест\\detect\\right side about 20 degree\\mic 1 - 02 (Right).wav'
+
+    angles = baseAlg(maxRangeCor, speed, frequency, dist, pathFirstMicro, pathSecondMicro)
+
+    return angles == 20
+
+def baseAlg(maxRangeCor, speed, frequency, dist, pathFirstMicro, pathSecondMicro):
     angles = list()
 
-    if difDist(maxRangeCor) > 0:
-        angles.append(m.degrees.cos(difDist(maxRangeCor) * u / (frequency * dist)))
+    if difDist(maxRangeCor, pathFirstMicro, pathSecondMicro) > 0:
+        angles.append(m.degrees(m.cos(difDist(maxRangeCor, pathFirstMicro, pathSecondMicro) * speed / (frequency * dist))))
         angles.append(
-            360 - m.degrees.cos(difDist(maxRangeCor) * u / (frequency * dist))
+            360 - m.degrees(m.cos(difDist(maxRangeCor, pathFirstMicro, pathSecondMicro) * speed / (frequency * dist)))
         )
 
-    angles.append(180 - m.degrees.cos(difDist(maxRangeCor) * u / (frequency * dist)))
-    angles.append(180 + m.degrees.cos(difDist(maxRangeCor) * u / (frequency * dist)))
+    angles.append(180 - m.degrees(m.cos(difDist(maxRangeCor, pathFirstMicro, pathSecondMicro) * speed / (frequency * dist))))
+    angles.append(180 + m.degrees(m.cos(difDist(maxRangeCor, pathFirstMicro, pathSecondMicro) * speed / (frequency * dist))))
 
     return angles
 
 
-def difDist(l):
-    x, sr = librosa.load(pathFirstMicro)
-    leftSignalFurie = rfft(x)
-    x, sr = librosa.load(pathSecondMicro)
-    rightSignalFurie = rfft(x)
+def difDist(l, pathFirstMicro, pathSecondMicro):
+    leftSignalFurie, sr = librosa.load(pathFirstMicro, sr=10000)
+    X = rfft(leftSignalFurie)
+    rightSignalFurie, sr = librosa.load(pathSecondMicro, sr=10000)
+    y = rfft(rightSignalFurie)
+    corrCoeff = np.corrcoef(X, y)
     best = 0
     dist = 0
 
-    for i in range(1, l):
-        corrCoeff = numpy.corrcoef(leftSignalFurie(i, l + i), rightSignalFurie(0, l))[
-            0, 1
-        ]
+    for i in range(0, l):
+        VAL = np.vstack((leftSignalFurie, rightSignalFurie))
+        corrCoeff= np.corrcoef(leftSignalFurie, rightSignalFurie)
+        corrCoeff = np.corrcoef(leftSignalFurie[i:l + i], rightSignalFurie[0:l])[0][1]
+
         if corrCoeff > best:
             best = corrCoeff
             dist = i
 
-    for i in range(1, l):
-        corrCoeff = numpy.corrcoef(leftSignalFurie(0, l), rightSignalFurie(i, l + i))[
-            0, 1
-        ]
+    for i in range(0, l):
+        corrCoeff = np.corrcoef(leftSignalFurie[0:l], rightSignalFurie[i: l + i])[0][1]
+
         if corrCoeff > best:
             best = corrCoeff
             dist = -1 * i
@@ -56,4 +63,4 @@ def difDist(l):
     return dist
 
 
-angles = baseAlg()
+angles = test()
