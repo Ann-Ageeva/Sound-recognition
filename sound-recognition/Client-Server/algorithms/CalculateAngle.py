3671fd87ce2915b61c90c11c9f8442ae5cd5e74e
Merilla <Shemet-mini@mail.ru>
2020-05-24 17:36:47 +0300

    Update to the latest version of CalculateAngle.py

--- sound-recognition/Client-Server/algorithms/CalculateAngle.py
+++ sound-recognition/Client-Server/algorithms/CalculateAngle.py
@@ -2,28 +2,14 @@ import math as m
 import numpy as np
 import librosa
 
-maxRangeCor = 1000  #Max correlation range
 speed = 330  #Sound speed
-frequency = 22000  #Sampling frequency
-dist = 100  #Distance between micros
+frequency = 44100  #Sampling frequency
+dist = 1  #Distance between micros
+maxRangeCor = int(dist / speed * frequency + 100)  #Max correlation range
 
-def baseAlg(pathFirstMicro, pathSecondMicro):
-    angles = list()
-    difDistValue=difDist(pathFirstMicro, pathSecondMicro)
-
-    if difDistValue > 0:
-        angles.append(m.degrees(m.cos(difDistValue)))
-        angles.append(360 - m.degrees(m.cos(difDistValue)))
-
-    angles.append(180 - m.degrees(m.cos(difDistValue)))
-    angles.append(180 + m.degrees(m.cos(difDistValue)))
-
-    return angles
-
-def difDist(pathFirstMicro, pathSecondMicro):
-    leftSignal, sr = librosa.load(pathFirstMicro, sr=48000)
-    rightSignal, sr = librosa.load(pathSecondMicro, sr=48000)
-
+def difDist(pathFirstMicro, pathSecondMicro):  
+    leftSignal, sr = librosa.load(pathFirstMicro, sr=frequency)
+    rightSignal, sr = librosa.load(pathSecondMicro, sr=frequency)
     sizeleft = leftSignal.size
     sizeright = rightSignal.size
     best = 0
@@ -43,6 +29,19 @@ def difDist(pathFirstMicro, pathSecondMicro):
             dist = -1 * i
 
     return dist
+
+def baseAlg(pathFirstMicro, pathSecondMicro):
+    angles = list()
+    difDistValue=(difDist(pathFirstMicro, pathSecondMicro) * speed) / (frequency * dist)
+
+    if difDistValue > 0:
+        angles.append(m.degrees(m.acos(difDistValue)))
+        angles.append(360 - m.degrees(m.acos(difDistValue)))
+
+    angles.append(180 - m.degrees(m.acos(difDistValue)))
+    angles.append(180 + m.degrees(m.acos(difDistValue)))
+
+    return angles
 
 def Calculate(pathFirstMicro, pathSecondMicro):
     angles = baseAlg(pathFirstMicro, pathSecondMicro)
